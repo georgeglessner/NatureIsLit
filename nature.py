2606834a3e8b59934875445feb5270f779833d36
George Glessner <glessneg@mail.gvsu.edu>
2018-02-17 00:14:08 -0500

    update for heroku

--- nature.py
+++ nature.py
@@ -28,40 +28,64 @@ if not os.path.exists('./images'):
     os.mkdir('./images')
 
 post_ids = []  # keeps track of used posts
-captions = []  # list of captions for images
+caption = ''  # caption for image
+extension = ''
 
 
 def get_images():
-    global post_ids, captions
+    global post_ids, caption
     count = 0
+    ids = []
+
+    with open('post_ids.txt') as f:
+        ids = f.readlines()
+    ids = [x.strip() for x in ids]
+
     # get 10 images from subreddit
     for submission in reddit.subreddit('NatureIsFuckingLit').hot():
-        if count < 10:
-            if 'https://i.imgur.com/' in submission.url or 'https://i.redd.it' in submission.url:
-                if str(submission.id) not in post_ids:
-                    img_url = submission.url
-                    _, extension = os.path.splitext(img_url)
-                    if extension in ['.jpg', '.gif', '.jpeg', '.png']:
-                        urllib.urlretrieve(
-                            img_url, 'images/%i%s' % (count, extension))
-                        count += 1
-                        captions.append(submission.title)
-                        post_ids.append(str(submission.id))
-        else:
-            break
-    send_tweet()
+        if 'https://i.imgur.com/' in submission.url or 'https://i.redd.it' in submission.url:
+            if str(submission.id) not in ids:
+                id_num = open('post_ids.txt', 'a')
+                id_num.write(str(submission.id + '\n'))
+                id_num.close()
+                img_url = submission.url
+                _, extension = os.path.splitext(img_url)
+                if extension == '.jpg':
+                    urllib.urlretrieve(
+                        img_url, 'images/image.jpg')
+                    caption = submission.title
+                    post_ids.append(str(submission.id))
+                    break
+                if extension == '.jpeg':
+                    urllib.urlretrieve(
+                        img_url, 'images/image.jpeg')
+                    caption = submission.title
+                    post_ids.append(str(submission.id))
+                    break
+                if extension == '.gif':
+                    urllib.urlretrieve(
+                        img_url, 'images/image.gif')
+                    caption = submission.title
+                    post_ids.append(str(submission.id))
+                    break
+                if extension == '.png':
+                    urllib.urlretrieve(
+                        img_url, 'images/image.png')
+                    caption = submission.title
+                    post_ids.append(str(submission.id))
+                    break
+
+    send_tweet(extension)
 
 
-def send_tweet():
-    global post_ids, captions
-    index = 0
-    for x in captions:
-        image = str(os.listdir("images")[index])
-        api.update_with_media("images/" + image, captions[index])
-        index += 1
-        sleep(1440)  # tweet every 4 hours
+def send_tweet(extension):
+    global post_ids, caption
+    api.update_with_media("images/image" + extension, caption)
+    print 'tweeting'
+    sleep(10800)  # tweet every 3 hours
     captions = []
     get_images()
+
 
 if __name__ == '__main__':
     get_images()
